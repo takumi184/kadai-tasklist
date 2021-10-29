a356cd1ab858a04ebbbebc4c97204d17e80c003b
takumi.kuwasaki <ee15317@gmail.com>
2021-10-29 07:37:07 +0000

    user signup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-    before_action :require_user_logged_in, only: [:index, :show]
     before_action :require_user_logged_in
     before_action :correct_user, only: [:destroy, :create, :edit, :show]
     before_action :set_message, only: [:show, :edit, :update, :destroy]
