296418c8d576aaacf02cb304a9b33a550f8bcfac
takumi.kuwasaki <ee15317@gmail.com>
2021-10-29 08:52:05 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :tasks
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
