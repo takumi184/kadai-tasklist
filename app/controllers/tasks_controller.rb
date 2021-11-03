02b24e6172dd8062bf4e735d19125c13c090057e
takumi.kuwasaki <ee15317@gmail.com>
2021-11-03 08:16:59 +0000

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
     before_action :correct_user, only: [:destroy,:edit, :show ,:update]
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
 
     def index
       @tasks = current_user.tasks
@@ -63,6 +62,10 @@ class TasksController < ApplicationController
     end
 
     def correct_user
+     @task = current_user.tasks.find_by(id: params[:id])
+     unless @task
+      redirect_to root_url
+     end
     end
     
     # Strong Parameter
